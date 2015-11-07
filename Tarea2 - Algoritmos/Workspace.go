4d6e75de97c8fa61aab3b6aab6a10ab3ad99635a
GuillermoIglesias <guillermo.iglesias.90@gmail.com>
2015-11-07 16:51:32 -0400

    Tarea2 Top-Down / Bottom-Up
    Terminadas funciones Top-Down / Bottom-Up

--- Tarea2 - Algoritmos/Workspace.go
+++ Tarea2 - Algoritmos/Workspace.go
@@ -14,7 +14,7 @@ func M_Factory(n, m int) [][]int64 {
 	}
 	for i := 0; i < n; i++ {
 		for j := 0; j < m; j++ {
-			Matrix[i][j] = rand.Int63n(10)
+			Matrix[i][j] = rand.Int63n(1) + 1
 		}
 	}
 	return Matrix
@@ -22,11 +22,7 @@ func M_Factory(n, m int) [][]int64 {
 
 func PrintMatrix(Matrix [][]int64) {
 	for i := 0; i < len(Matrix); i++ {
-		fmt.Print("[")
-		for j := 0; j < len(Matrix[0]); j++ {
-			fmt.Printf("%6d", Matrix[i][j])
-		}
-		fmt.Println("]")
+		fmt.Println(Matrix[i])
 	}
 }
 
@@ -51,11 +47,9 @@ func Naive(M [][][]int64, cost int64) {
 
 	if len(M) == 1 {
 		fmt.Println("Costo Total:", cost)
-		//PrintMatrix(M[0])
 		return
 	}
 
-	fmt.Println("Value ->", (len(M[0]) * len(M[0][0]) * len(M[1][0])))
 	cost += int64((len(M[0]) * len(M[0][0]) * len(M[1][0])))
 
 	MxM := Multiplicador(M[0], M[1])
@@ -75,7 +69,6 @@ func Greedy(M [][][]int64, cost int64) {
 
 	if len(M) == 1 {
 		fmt.Println("Costo Total:", cost)
-		//PrintMatrix(M[0])
 		return
 	}
 
@@ -86,8 +79,6 @@ func Greedy(M [][][]int64, cost int64) {
 			save = i
 		}
 	}
-
-	fmt.Println("Value ->", value)
 
 	cost += value
 
@@ -101,9 +92,8 @@ func Greedy(M [][][]int64, cost int64) {
 	Greedy(M[:(len(M)-1)], cost)
 }
 
-func MatrixChainOrder(M [][][]int64) int64 {
-	/* For simplicity of the program, one extra row and one extra column are
-	allocated in m[][].  0th row and 0th column of m[][] are not used */
+func Top_Down(M [][][]int64) int64 {
+
 	n := (len(M) + 1)
 
 	m := make([][]int64, n)
@@ -113,18 +103,46 @@ func MatrixChainOrder(M [][][]int64) int64 {
 		s[i] = make([]int64, n)
 	}
 
-	/* m[i,j] = Minimum number of scalar multiplications needed to compute
-	   the matrix A[i]A[i+1]...A[j] = A[i..j] where dimention of A[i] is
-	   p[i-1] x p[i] */
-	// cost is zero when multiplying one matrix.
+	cost := Top_Down_Func(M, m, s, 1, int64(len(M)))
 
-	// L is chain length.
+	PrintMatrix(Mult(M, s, 1, int64(len(M))))
+
+	return cost
+}
+
+func Top_Down_Func(M [][][]int64, m, s [][]int64, i, j int64) int64 {
+	if i == j {
+		return 0
+	}
+
+	m[i][j] = math.MaxInt64
+
+	for k := i; int64(k) <= j-1; k++ {
+		q := Top_Down_Func(M, m, s, i, k) + Top_Down_Func(M, m, s, k+1, j) + int64(len(M[i-1])*len(M[k-1][0])*len(M[j-1][0]))
+		if q < m[i][j] {
+			m[i][j] = q
+			s[i][j] = int64(k)
+		}
+	}
+
+	return m[i][j]
+}
+
+func Bottom_Up(M [][][]int64) int64 {
+	n := (len(M) + 1)
+
+	m := make([][]int64, n)
+	s := make([][]int64, n)
+	for i := 0; i < n; i++ {
+		m[i] = make([]int64, n)
+		s[i] = make([]int64, n)
+	}
+
 	for L := 2; L < n; L++ {
 		for i := 1; i < n-L+1; i++ {
 			j := int64(i + L - 1)
 			m[i][j] = math.MaxInt64
 			for k := i; int64(k) <= j-1; k++ {
-				// q = cost/scalar multiplications
 				q := m[i][k] + m[k+1][j] + int64(len(M[i-1])*len(M[k-1][0])*len(M[j-1][0]))
 				if q < m[i][j] {
 					m[i][j] = q
@@ -134,8 +152,7 @@ func MatrixChainOrder(M [][][]int64) int64 {
 		}
 	}
 
-	//PrintMatrix(Mult(M, s, 1, int64(n-1)))
-	Mult(M, s, 1, int64(n-1))
+	PrintMatrix(Mult(M, s, 1, int64(len(M))))
 	return m[1][n-1]
 }
 
@@ -154,31 +171,43 @@ func main() {
 
 	rand.Seed(time.Now().UnixNano())
 
-	A := M_Factory(3000, 350)
-	B := M_Factory(350, 1500)
-	C := M_Factory(1500, 500)
-	D := M_Factory(500, 1000)
-	E := M_Factory(1000, 2000)
-	F := M_Factory(2000, 2500)
+	A := M_Factory(3, 2)
+	B := M_Factory(2, 1)
+	C := M_Factory(1, 5)
+	D := M_Factory(5, 2)
+	E := M_Factory(2, 2)
+	F := M_Factory(2, 2)
+	G := M_Factory(2, 1)
+	H := M_Factory(1, 5)
+	I := M_Factory(5, 3)
+	J := M_Factory(3, 3)
 
-	Matrices := [][][]int64{A, B, C, D, E, F}
+	//Matrices := [][][]int64{A, B, C, D}
+	Matrices := [][][]int64{A, B, C, D, E, F, G, H, I, J}
 
 	Aux1 := make([][][]int64, len(Matrices))
 	Aux2 := make([][][]int64, len(Matrices))
 	Aux3 := make([][][]int64, len(Matrices))
+	Aux4 := make([][][]int64, len(Matrices))
 	copy(Aux1, Matrices[0:])
 	copy(Aux2, Matrices[0:])
 	copy(Aux3, Matrices[0:])
+	copy(Aux4, Matrices[0:])
 
 	fmt.Println("Naive:")
 	Naive(Aux1, 0)
+	PrintMatrix(Aux1[0])
 	fmt.Println("---------")
 
 	fmt.Println("Greedy:")
 	Greedy(Aux2, 0)
+	PrintMatrix(Aux2[0])
 	fmt.Println("---------")
 
-	fmt.Println("Matrix Chain Order:")
-	fmt.Println("Costo Minimo:", MatrixChainOrder(Aux3))
+	fmt.Println("Bottom-Up:")
+	fmt.Println("Costo Total:", Bottom_Up(Aux3))
 	fmt.Println("---------")
+
+	fmt.Println("Top-Down:")
+	fmt.Println("Costo Total:", Top_Down(Aux4))
 }
